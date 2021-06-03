237effb87c8d9f7b3707c83fb5fbe910cbe2b076
ceichert <ceichert84@gmail.com>
2021-06-03 12:50:49 +0200

    initial commit

---
+++ src/main/java/it/eichert/springbootcameltemplate/SpringBootCamelTemplateApplication.java
@@ -0,0 +1,13 @@
+package it.eichert.springbootcameltemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCamelTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCamelTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/eichert/springbootcameltemplate/SpringBootCamelTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package it.eichert.springbootcameltemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCamelTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
